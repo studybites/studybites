37a69ab57a70b53894e51b1d9f7dcbaa0c44e0d8
Kamal Mukkamala <kamalnrf@gmail.com>
2021-04-04 13:37:36 +0530

    Add button componet

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'
+import './App.css'
+import Button from './components/button'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="outline">Button</Button>
+      </header>
+    </div>
+  )
+}
+
+export default App
